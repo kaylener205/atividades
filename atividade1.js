8b753fad56dc8fa68052b36dfc74613b4d3a046f
Kaylene <kayleneresende@gmail.com>
2026-06-22 15:10:27 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,13 @@
+const usuario = {
+  nome: prompt("Digite seu nome:"),
+  idade: prompt("Digite sua idade:"),
+  email: prompt("Digite seu e-mail:"),
+  telefone: prompt("Digite seu telefone:")
+};
+
+alert(
+  "=== DADOS DO USUÁRIO ===\n" +
+  "Nome: " + usuario.nome + "\n" +
+  "Idade: " + usuario.idade + "\n" +
+  "E-mail: " + usuario.email + "\n" +
+  "Telefone: " + usuario.telefone
